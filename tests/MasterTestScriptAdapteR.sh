3f8a9469e3faf66eb0f51e3de65783d20fcbc1a8
phanisrikar93 <phanisrikar93ume@gmail.com>
2017-03-07 10:09:46 +0530

    smash commit later

--- tests/MasterTestScriptAdapteR.sh
+++ tests/MasterTestScriptAdapteR.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-#MasterTestScriptAdapteR.sh "TD" "ODBC" "C:/Users/phani/Documents/Shell/connectionConfig.txt" "Q:/AdapteR/tests"
-#MasterTestScriptAdapteR.sh "TD" "JDBC" "C:/Users/phani/Documents/Shell/connectionConfig.txt" "Q:/AdapteR/tests" "C:/Users/phani/Downloads"
+#MasterTestScriptAdapteR.sh "TD" "ODBC" "C:/Users/phani/Documents/Shell/connectionConfig.txt" "Q:/AdapteR/tests" "" "" "C:/Users/phani/Desktop"
+#MasterTestScriptAdapteR.sh "TD" "JDBC" "C:/Users/phani/Documents/Shell/connectionConfig.txt" "Q:/AdapteR/tests" "C:/Users/phani/Downloads" "" "C:/Users/phani/Desktop"
 export IFS=","
 FILEPATH=$3
 cd $4
+INSTALL_FILE=`mktemp`
+echo $'remove.packages("AdapteR")
+library(devtools)
+install_github("Fuzzy-Logix/AdapteR",ref="$branch")' > $INSTALL_FILE
+Rscript $INSTALL_FILE
+rm $INSTALL_FILE
 cat $FILEPATH | while read platform ctype dsn DB host usr pwd
 do
     echo "$platform,$ctype,$dsn,$DB,$usr,$pwd,$host"
     if [ "$2" = "ODBC" ] && [ "$1" = "$platform" ] && [ "$ctype" = "$2" ]
     then
-        Rscript run_testsuite.R -H "$dsn" -A "require" -d ".." -P "$platform" -D "$DB"
+        Rscript run_testsuite.R -H "$dsn" -A "require" -d ".." -P "$platform" -D "$DB" -R $6
     elif [ "$2" = "JDBC" ] && [ "$1" = "$platform" ] && [ "$ctype" = "$2" ]
     then
-        Rscript run_testsuite.R -H "$host" -A "require" -d ".." -P "$platform" -u "$usr" -p "$pwd" -D "$DB" -J "$5" -j "$6"
+        Rscript run_testsuite.R -H "$host" -A "require" -d ".." -P "$platform" -u "$usr" -p "$pwd" -D "$DB" -J "$5" -j "$6" -R $6
     fi
 done
--- tests/runFullPlatformTests.sh
+++ tests/runFullPlatformTests.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 #Args:ScriptsPath, JarsPath, gitBranchToTest, resultsPath
-#runFullPlatformTests.sh  "P:/" "C:/Users/phani/Downloads" "AutoTesting"
+#runFullPlatformTests.sh  "P:/" "C:/Users/phani/Downloads" "AutoTesting" "C:/Users/phani/Desktop"
+#To run only ODBC, edit the config file to contain only ODBC entries and use:
+#runFullPlatformTests.sh  "P:/" "" "AutoTesting" ""
 export IFS=","
 cd $1
 rm -rf AdapteR
 branch="master"
+resultsPath=$1
 if [ ! -z $3 ]
 then
    branch=$3
+fi
+if [ ! -z $4 ]
+then
+   resultsPath=$4
 fi
 git clone -b $branch https://github.com/phanisrikar93/AdapteR
 cd "AdapteR/tests"
@@ -16,6 +23,6 @@ configFILE="$FILESPATH/connectionConfig.txt"
 cat "$configFILE" | while read platform ctype dsn DB host usr pwd
 do
     echo -e " \n \n ######### Testing AdapteR on : $platform : using : $ctype : ########### \n \n "
-    ./MasterTestScriptAdapteR.sh "$platform" "$ctype" "$configFILE" "$FILESPATH" "$2"
+    ./MasterTestScriptAdapteR.sh "$platform" "$ctype" "$configFILE" "$FILESPATH" "$2" "$resultsPath"
     echo -e " \n \n ######### END Testing AdapteR on : $platform : using : $ctype : END END : ############## \n \n "
 done
